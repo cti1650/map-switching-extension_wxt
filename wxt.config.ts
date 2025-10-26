c5155d8cb6af3f858325e976b44c49eff8847113
cti1650 <cti1650@gmail.com>
2025-10-26 14:24:43 +0900

    chore: 不要になったモジュールを削除（標準機能で対応可能だった）

--- wxt.config.ts
+++ wxt.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     description: "Map Switching Extension",
     permissions: ["tabs"],
   },
-  modules: ["@wxt-dev/module-react", "@wxt-dev/auto-icons", "./src/modules/sync-version.ts"],
+  modules: ["@wxt-dev/module-react", "@wxt-dev/auto-icons"],
   alias: {
     $component: path.resolve("./src/component"),
     $hooks: path.resolve("./src/hooks"),
