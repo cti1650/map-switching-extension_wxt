3573bf98f2cb2a76c6ac183b7cb8808f7ec430d4
cti1650 <cti1650@gmail.com>
2025-10-27 22:45:20 +0900

    feat: デフォルトのブラウザ設定を追加

---
+++ web-ext.config.ts
@@ -0,0 +1,7 @@
+import { defineWebExtConfig } from "wxt";
+
+export default defineWebExtConfig({
+  startUrls: [
+    "https://www.google.com/maps/@35.709635,139.8102033,3a,90y,38.01h,104.06t/data=!3m7!1e1!3m5!1sBoSfdWvSXZnysLjeVdnecQ!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fcb_client%3Dmaps_sv.tactile%26w%3D900%26h%3D600%26pitch%3D-14.061108908383915%26panoid%3DBoSfdWvSXZnysLjeVdnecQ%26yaw%3D38.00537503926333!7i16384!8i8192?authuser=0&entry=ttu&g_ep=EgoyMDI1MTAyMi4wIKXMDSoASAFQAw%3D%3D",
+  ],
+});
