462dbb70a4d95d30839f1cbee04fd307b8788553
cti1650 <cti1650@gmail.com>
2025-11-09 04:20:46 +0900

    feat: Playwrightの失敗時にスクリーンショットを残すように変更

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,6 +18,10 @@ export default defineConfig({
   use: {
     // Collect trace when retrying the failed test.
     trace: "on-first-retry",
+    // Record video only on failure
+    video: "retain-on-failure",
+    // Take screenshot only on failure
+    screenshot: "only-on-failure",
   },
 
   // Configure projects for major browsers.
