409bb49ba8f1a0f9b6a06da01babc53f94b710ef
cti1650 <cti1650@gmail.com>
2025-12-03 01:58:10 +0900

    fix: 川の防災情報のE2Eテストを修正

--- tests/e2e/pom/kawabou.page.ts
+++ tests/e2e/pom/kawabou.page.ts
@@ -6,9 +6,7 @@ export class KawabouPage extends BasePage {
    * 検索結果のテキストを取得
    */
   async getSearchResultText(): Promise<string | null> {
-    const searchResult = await this.page.waitForSelector("#pcMapHeader .subtitle-1.success--text", {
-      state: "attached",
-    });
+    const searchResult = await this.page.waitForSelector("#pcMapHeader .subtitle-1.success--text");
     if (searchResult) {
       return await searchResult.textContent();
     }
